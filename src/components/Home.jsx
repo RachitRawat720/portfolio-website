dd4cda2cd976b4a9cf163fd1c57289f3100dacad
Rachit Singh Rawat <rachitrawat720@gmail.com>
2026-09-02 21:54:11 +0530

    feat: update hero section layout and styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,10 +6,14 @@ import { faArrowRight, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import TypingText from './Typingtext';
 import linkedin_logo from '../assets/linkedIn_logo.png'
 import github_logo from '../assets/github_logo.jpg'
+import react_img from '../assets/react-img.jpg'
+import node_img from '../assets/node-img.png'
+import express_img from '../assets/express-img.png'
+import mongoDB_img from '../assets/mongoDB-img.png'
 
 const Home = () => {
     return (
-        <div className='flex px-10 py-8 gap-5 bg-[linear-gradient(135deg,#080D1F_0%,#101A35_35%,#172554_65%,#24164F_100%)]'>
+        <div className='flex relative px-10 py-8 gap-5 bg-[linear-gradient(135deg,#080D1F_0%,#101A35_35%,#172554_65%,#24164F_100%)]'>
             <div className='w-1/2'>
                 <div className='flex'>
                         <img src={hi} className='h-7 w-9 mt-2'/>
@@ -27,13 +31,14 @@ const Home = () => {
                     <p className='text-[18px] mt-6 text-gray-200'>I'm passionate about building clean, responsive, and interactive web experiences. I use React, JavaScript, Tailwind CSS, and modern web technologies to turn ideas into engaging digital experiences.</p>
                 </div>
 
-                <div className='flex gap-6 mt-10'>
+                <div className='flex gap-12 mt-10 items-center'>
                     <a href='/Projects'><button className='bg-linear-to-r from-[#1E40AF] via-[#4338CA] to-[#7C3AED] text-white px-1 py-1 rounded-md flex gap-2 justify-center items-center w-40 h-13 text-[16px] hover:cursor-pointer transition-transform duration-300 ease-in-out hover:-translate-y-3'>View My Work<FontAwesomeIcon icon={faArrowRight} /></button></a>
 
-                    <button className='border-2 border-[#7C3AED] text-white px-1 py-1 rounded-md gap-2 flex justify-center items-center w-40 h-13 text-[16px] hover:bg-linear-to-r from-[#1E40AF] via-[#4338CA] to-[#7C3AED] hover:border-none hover:cursor-pointer transition-transform duration-300 ease-in-out hover:-translate-y-3'>View Resume<FontAwesomeIcon icon={faArrowRight}/></button>
-
                     <a href='/Contact'><button className='border-2 border-[#7C3AED] text-white px-1 py-1 rounded-md gap-2 flex justify-center items-center w-40 h-13 text-[16px] hover:bg-linear-to-r from-[#1E40AF] via-[#4338CA] to-[#7C3AED] hover:border-none hover:cursor-pointer transition-transform duration-300 ease-in-out hover:-translate-y-3'>Contact Me<FontAwesomeIcon icon= {faEnvelope}></FontAwesomeIcon></button></a>
+
+                    <button className='border-2 border-[#7C3AED] text-white px-1 py-1 rounded-md gap-2 flex justify-center items-center w-30 h-10 text-[13px] hover:bg-linear-to-r from-[#1E40AF] via-[#4338CA] to-[#7C3AED] hover:border-none hover:cursor-pointer transition-transform duration-300 ease-in-out hover:-translate-y-3'>View Resume<FontAwesomeIcon icon={faArrowRight}/></button>
                 </div>
+
                 
                 <div className='flex mt-8 gap-7'>
                     <a href='https://www.linkedin.com/in/rachit-singh-rawat/' target='_blank' rel="noopener noreferrer" className='group block '><div className='w-11 h-11 border-2 border-[#3e536a] bg-linear-to-br from-[#2b3b4c] to-[#212e3f] rounded-md flex justify-center items-center transition-transform duration-300 ease-in-out group-hover:scale-150'><img src={linkedin_logo} className='w-7 h-7 rounded-md' alt="" /></div></a>
@@ -44,6 +49,22 @@ const Home = () => {
 
             <div className='w-1/2'>
                 <img src={profile_pic} className='mt-20 rounded-4xl w-full h-auto mask-[radial-gradient(ellipse_at_center,black_55%,transparent_100%)]'/>
+            </div>
+
+            <div className='absolute h-20 w-20 top-34 left-175 overflow-hidden rounded-lg transition-transform duration-500 hover:rotate-180'>
+                <img src={react_img} alt="" className='h-full w-full'/>
+            </div>
+
+            <div className='absolute h-20 w-20 top-80 left-165 overflow-hidden rounded-lg transition-transform duration-500 hover:rotate-180'>
+                <img src={node_img} alt="" className='h-full w-full'/>
+            </div>
+
+            <div className='absolute h-20 w-20 top-45 left-275 overflow-hidden rounded-lg transition-transform duration-500 hover:rotate-180'>
+                <img src={express_img} alt="" className='h-full w-full'/>
+            </div>
+
+            <div className='absolute h-20 w-20 top-95 left-275 overflow-hidden rounded-lg transition-transform duration-500 hover:rotate-180'>
+                <img src={mongoDB_img} alt="" className='h-full w-full'/>
             </div>
         </div>
     )
