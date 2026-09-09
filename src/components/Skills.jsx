539bd0aba0ad5be4674c185abdea4ab31437f7f6
Rachit Singh Rawat <rachitrawat720@gmail.com>
2026-09-09 17:06:27 +0530

    completing skills page

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -86,11 +86,12 @@ const Skills = () => {
                 </motion.div>
             </div>
 
-            <motion.div initial={{opacity: 0, y: -300}} whileInView={{opacity: 1, y: 0}}
+            <div className='flex items-center justify-center'>
+                <motion.div initial={{opacity: 0, y: -300}} whileInView={{opacity: 1, y: 0}}
                 transition={{duration: 2, delay: 2}} viewport={{once: true, amount: 0.9}}
-                className='border-2 border-[#3B82F6] bg-[#111B36] px-10 py-5 rounded-xl w-190 h-35 mt-40 ml-52'>
+                className='border-2 border-[#3B82F6] bg-[#111B36] px-10 py-5 rounded-xl w-130 h-60 mt-40 flex flex-col'>
                     <h1 className='text-[26px] text-[#F8FAFC] font-semibold flex itemsj-center justify-center'>Development Tools</h1>
-                    <div className='flex justify-center items-center mt-5 gap-5'>
+                    <div className='grid grid-cols-2 mt-5 gap-5'>
                         
                         <p className='text-[#A78BFA] border-2 border-[#8B5CF6] w-fit px-5 rounded-2xl hover:text-[#FFFFFF] hover:bg-[#695694]/50'>Git</p>
 
@@ -102,65 +103,72 @@ const Skills = () => {
 
                         <p className='text-[#A78BFA] border-2 border-[#8B5CF6] w-fit px-5 rounded-2xl hover:text-[#FFFFFF] hover:bg-[#695694]/50'>Vercel</p>
                     </div>
-            </motion.div>
+                </motion.div>
+            </div>
 
             <div className='flex flex-col items-center justify-center w-full mt-30'>
-                <motion.div initial={{opacity: 0, y: -300}} whileInView={{opacity: 1, y: 0}}
-                transition={{duration: 2, delay: 2}} viewport={{once: true, amount: 0.9}}>
+                <motion.div initial={{opacity: 0, y: 300}} whileInView={{opacity: 1, y: 0}}
+                transition={{duration: 2}} viewport={{once: true, amount: 0.1}} className='flex flex-col justify-center items-center'>
                     <h1 className='text-[40px] font-bold bg-linear-to-r from-[#3B82F6] via-[#8B5CF6] to-[#EC4899] bg-clip-text text-transparent'>MY PROFICIENCY</h1>
                     <div className='bg-linear-to-r from-[#3B82F6] via-[#8B5CF6] to-[#EC4899]  h-1 w-30 rounded-4xl'></div>
-                    <h3 className='text-[25px] text-gray-300 mt-10'>Skills Levels</h3>
+                    <h3 className='text-[28px] text-gray-300 mt-10'>Skills Levels</h3>
                 </motion.div>
 
-                <div initial={{opacity: 0, y: -300}} whileInView={{opacity: 1, y: 0}} transition={{duration: 2, delay: 2}} viewport={{once: true, amount: 0.9}}>
-                    <div className='flex items-center justify-center mt-25 gap-10'>
-                        <p className='text-[20px] text-gray-300'>React.js</p>
-                        <div className='w-120 h-6 rounded-2xl border-4 border-[#8B5CF6]'>
-                            <div className='w-100 h-full rounded-l-2xl  bg-[#995778]'></div>
+                <div className='flex gap-15 justify-between pt-40 pb-20'>
+                    <motion.div initial={{opacity: 0, x: -200}} whileInView={{opacity: 1, x: 0}} transition={{duration: 2}} viewport={{once: true, amount: 0.7}}
+                    className='flex flex-col gap-8'>
+                        <div className='flex gap-15 hover:scale-115'>
+                            <p className='text-[20px] text-[#E2E8F0] font-medium'>React.js</p>
+                            <div className='w-100 h-7 rounded-full bg-[#1E293B] border border-[#334155] overflow-hidden'>
+                                <div className='w-[85%] h-full rounded-l-2xl bg-linear-to-r from-[#3B82F6] via-[#6366F1] to-[#8B5CF6] shadow-[0_0_12px_#6366F180]'></div>
+                            </div>
                         </div>
-                    </div>
 
-                    <div className='flex items-center justify-center mt-10 gap-10'>
-                        <p className='text-[20px] text-gray-300'>JavaScript</p>
-                        <div className='w-120 h-6 rounded-2xl border-4 border-[#8B5CF6]'>
-                            <div className='w-90 h-full rounded-l-2xl  bg-[#995778]'></div>
+                        <div className='flex gap-9 hover:scale-115'>
+                            <p className='text-[20px] text-[#E2E8F0] font-medium'>JavaScript</p>
+                            <div className='w-100 h-7 rounded-full bg-[#1E293B] border border-[#334155] overflow-hidden'>
+                                <div className='w-[90%] h-full rounded-l-2xl bg-linear-to-r from-[#3B82F6] via-[#6366F1] to-[#8B5CF6] shadow-[0_0_12px_#6366F180]'></div>
+                            </div>
                         </div>
-                    </div>
 
-                    <div className='flex items-center justify-center mt-10 gap-10'>
-                        <p className='text-[20px] text-gray-300'>Tailwind CSS</p>
-                        <div className='w-120 h-6 rounded-2xl border-4 border-[#8B5CF6]'>
-                            <div className='w-100 h-full rounded-l-2xl  bg-[#995778]'></div>
+                        <div className='flex gap-4 hover:scale-115'>
+                            <p className='text-[20px] text-[#E2E8F0] font-medium'>Tailwind CSS</p>
+                            <div className='w-100 h-7 rounded-full bg-[#1E293B] border border-[#334155] overflow-hidden'>
+                                <div className='w-[80%] h-full rounded-l-2xl bg-linear-to-r from-[#3B82F6] via-[#6366F1] to-[#8B5CF6] shadow-[0_0_12px_#6366F180]'></div>
+                            </div>
                         </div>
-                    </div>
 
-                    <div className='flex items-center justify-center mt-10 gap-10'>
-                        <p className='text-[20px] text-gray-300'>Node.js</p>
-                        <div className='w-120 h-6 rounded-2xl border-4 border-[#8B5CF6]'>
-                            <div className='w-100 h-full rounded-l-2xl  bg-[#995778]'></div>
+                        <div className='flex gap-14 hover:scale-115'>
+                            <p className='text-[20px] text-[#E2E8F0] font-medium'>Node.js</p>
+                            <div className='w-100 h-7 rounded-full bg-[#1E293B] border border-[#334155] overflow-hidden'>
+                                <div className='w-[80%] h-full rounded-l-2xl bg-linear-to-r from-[#3B82F6] via-[#6366F1] to-[#8B5CF6] shadow-[0_0_12px_#6366F180]'></div>
+                            </div>
                         </div>
-                    </div>
+                    </motion.div>
 
-                    <div className='flex items-center justify-center mt-10 gap-10'>
-                        <p className='text-[20px] text-gray-300'>Express.js</p>
-                        <div className='w-120 h-6 rounded-2xl border-4 border-[#8B5CF6]'>
-                            <div className='w-100 h-full rounded-l-2xl  bg-[#995778]'></div>
+                    <motion.div initial={{opacity: 0, x: 200}} whileInView={{opacity: 1, x: 0}} transition={{duration: 2}} viewport={{once: true, amount: 0.7}}
+                    className='flex flex-col gap-8'>
+                        <div className='flex gap-8 hover:scale-115'>
+                            <p className='text-[20px] text-[#E2E8F0] font-medium'>Express.js</p>
+                            <div className='w-100 h-7 rounded-full bg-[#1E293B] border border-[#334155] overflow-hidden'>
+                                <div className='w-[85%] h-full rounded-l-2xl bg-linear-to-r from-[#3B82F6] via-[#6366F1] to-[#8B5CF6] shadow-[0_0_12px_#6366F180]'></div>
+                            </div>
                         </div>
-                    </div>
 
-                    <div className='flex items-center justify-center mt-10 gap-10'>
-                        <p className='text-[20px] text-gray-300'>MongoDB</p>
-                        <div className='w-120 h-6 rounded-2xl border-4 border-[#8B5CF6]'>
-                            <div className='w-100 h-full rounded-l-2xl  bg-[#995778]'></div>
+                        <div className='flex gap-7 hover:scale-115'>
+                            <p className='text-[20px] text-[#E2E8F0] font-medium'>MongoDB</p>
+                            <div className='w-100 h-7 rounded-full bg-[#1E293B] border border-[#334155] overflow-hidden'>
+                                <div className='w-[70%] h-full rounded-l-2xl bg-linear-to-r from-[#3B82F6] via-[#6366F1] to-[#8B5CF6] shadow-[0_0_12px_#6366F180]'></div>
+                            </div>
                         </div>
-                    </div>
 
-                    <div className='flex items-center justify-center mt-10 gap-10'>
-                        <p className='text-[20px] text-gray-300'>MySQL</p>
-                        <div className='w-120 h-6 rounded-2xl border-4 border-[#8B5CF6]'>
-                            <div className='w-100 h-full rounded-l-2xl  bg-[#995778]'></div>
+                        <div className='flex gap-13 hover:scale-115'>
+                            <p className='text-[20px] text-[#E2E8F0] font-medium'>MySQL</p>
+                            <div className='w-100 h-7 rounded-full bg-[#1E293B] border border-[#334155] overflow-hidden'>
+                                <div className='w-[60%] h-full rounded-l-2xl bg-linear-to-r from-[#3B82F6] via-[#6366F1] to-[#8B5CF6] shadow-[0_0_12px_#6366F180]'></div>
+                            </div>
                         </div>
-                    </div>
+                    </motion.div>
                 </div>
             </div>
         </div>
